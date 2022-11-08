9185962c3e8f265fd02e763aa0d5a1e8c0bf1025
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-08 23:35:05 +0200

    add Statistics jsx

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Profile } from 'components/profile/Profile';
+import { Statistics } from 'components/statistics/Statistics';
 import './index.css';
 import user from "./path/to/user.json";
+import data from "./path/to/data.json";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -15,6 +17,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
+      />
+      <Statistics 
+        // title='asdasdasdasd'
+        stats={data}
       />
     </App>
   </React.StrictMode>
