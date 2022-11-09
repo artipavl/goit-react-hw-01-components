f06a8cfd89bac02d52c060fb8588d3113aee005b
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-09 20:16:15 +0200

    add style on friend

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,8 +1,9 @@
 import { FriendListItem } from "components/friendListItem/FriendListItem";
+import css from "components/friendList/FriendList.module.css";
 
 export const FriendList = ({friends}) => {
     return (
-        <ul className="friend-list">
+        <ul className={css.friendList}>
             {friends.map(frend => <FriendListItem key={frend.id}
                 avatar={frend.avatar} name={frend.name} isOnline={frend.isOnline}
             />)}
--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,9 +1,14 @@
+import css from "components/friendListItem/FriendListItem.mobule.css";
+
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className="item">
-      <span className="status">{isOnline ? 'online': 'ofline'}</span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
+    <li className={css.item} >
+      <span className={css.status} style={
+        isOnline ? {background: "green"} : {background: "red"}
+      }
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
