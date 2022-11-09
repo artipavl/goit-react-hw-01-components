abcc93299916f62355f2dc4d027cb2a0e3d27dcd
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-09 20:39:52 +0200

    add css on TransactionHistory

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,7 +1,9 @@
+import css from "components/transactionHistory/TransactionHistory.module.css";
+
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
+    <table className={css.transactionHistory}>
+      <thead className={css.transaction}>
         <tr>
           <th>Type</th>
           <th>Amount</th>
@@ -12,7 +14,7 @@ export const TransactionHistory = ({ items }) => {
       <tbody>
         {items.map(({id, type, amount, currency}) => {
           return (
-            <tr key={id}>
+            <tr key={id} className={css.transactionTr}>
               <td>{type}</td>
               <td>{amount}</td>
               <td>{currency}</td>
